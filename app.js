f2983d8f0114a42b70edc9579567d48a2a5387be
Will <willpearson9120@gmail.com>
2024-11-13 16:27:48 +0000

    Final commit

--- app.js
+++ app.js
@@ -1,152 +1,127 @@
-// Main
-"use strict";
-
-/*
-** The Gameboard represents the state of the board
-** Each equare holds a Cell (defined later)
-
-*/
-
-const Gameboard = function () {
-  const rows = 3;
-  const cols = 3;
-  const board = [];
-
-  for (let i = 0; i < rows; i++) {
-    board[i] = [];
-    for (let j = 0; j < cols; j++) {
-      board[i].push(Cell());
-      // board[i].push("cell");
-    }
-  }
-
-  const getBoard = () => board;
-
-  const drawMarker = (column, player) => {
-    const availableCells = board
-      .filter((row) => !row[column].getValue())
-      .map((row) => row[column]);
-
-    if (!availableCells.length) return;
-    availableCells[row][column].addMarker(player.marker);
+const displayController = (() => {
+  const renderMessage = (message) => {
+    document.querySelector("#message").innerHTML = message;
   };
 
-  const printBoard = () => {
-    const boardWithCellValues = board.map((row) =>
-      row.map((cell) => cell.getValue())
-    );
-    console.log(boardWithCellValues);
-  };
-  return {
-    getBoard,
-    drawMarker,
-    printBoard,
-  };
-};
+  return { renderMessage };
+})();
 
-function Cell() {
-  let value;
+const Gameboard = (() => {
+  let gameboard = ["", "", "", "", "", "", "", "", ""];
 
-  const addMarker = (playerMarker) => {
-    value = playerMarker;
-  };
-
-  const getValue = () => value;
-
-  return {
-    addMarker,
-    getValue,
-  };
-}
-
-function GameController(
-  playerOneName = "Player One",
-  playerTwoName = "Player Two"
-) {
-  const board = Gameboard();
-
-  const players = [
-    {
-      name: playerOneName,
-      marker: "X",
-    },
-    {
-      name: playerTwoName,
-      marker: "O",
-    },
-  ];
-
-  let activePlayer = players[0];
-
-  const switchPlayerTurn = () => {
-    activePlayer = activePlayer === players[0] ? players[1] : players[0];
-  };
-
-  const getActivePlayer = () => activePlayer;
-
-  const printNewRound = () => {
-    board.printBoard();
-    console.log(`${getActivePlayer().name}'s turn`);
-  };
-
-  const playRound = () => {
-    board.drawMarker(getActivePlayer().marker);
-
-    /*  This is where we would check for a winner and handle that logic, such as a win message. */
-
-    switchPlayerTurn();
-    printNewRound();
-  };
-  printNewRound();
-
-  return {
-    playRound,
-    getActivePlayer,
-    getBoard: board.getBoard,
-  };
-}
-
-function ScreenController() {
-  const game = GameController();
-  const playerTurnDiv = document.querySelector(".turn");
-  const boardDiv = document.querySelector(".board");
-
-  const updateScreen = () => {
-    // clear the board
-    boardDiv.textContent = "";
-
-    // get the newest version of the board and player turn
-    const board = game.getBoard();
-    const activePlayer = game.getActivePlayer();
-
-    // display player's turn
-    playerTurnDiv.textContent = `${activePlayer.name}'s turn...`;
-
-    // render board squares
-    board.forEach((row) => {
-      row.forEach((cell) => {
-        const cellButton = document.createElement("button");
-        cellButton.classList.add("cell");
-        // // Create a data attribute to identify the column
-        // // This makes it easier to pass into our `playRound` function
-        // cellButton.dataset.column = index;
-        // cellButton.textContent = cell.getValue();
-        // boardDiv.appendChild(cellButton);
-      });
+  const render = () => {
+    let boardHTML = "";
+    gameboard.forEach((square, index) => {
+      boardHTML += `<div class="square" id="square-${index}">${square}</div>`;
+    });
+    document.querySelector("#gameboard").innerHTML = boardHTML;
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+      square.addEventListener("click", Game.handleClick);
     });
   };
 
-  function clickHandlerBoard(e) {
-    const selectedCell = e.target;
-    game.playRound(selectedCell);
-    updateScreen();
+  const update = (index, value) => {
+    gameboard[index] = value;
+    render();
+  };
+
+  // Allows access to gameboard without ability to modfy it
+  const getGameboard = () => gameboard;
+
+  return {
+    render,
+    update,
+    getGameboard,
+  };
+})();
+
+const createPlayer = (name, mark) => {
+  return { name, mark };
+};
+
+const Game = (() => {
+  let players = [];
+  let currentPlayerIndex;
+  let gameOver;
+
+  const start = () => {
+    players = [
+      createPlayer(document.querySelector("#player1").value, "X"),
+      createPlayer(document.querySelector("#player2").value, "O"),
+    ];
+    currentPlayerIndex = 0;
+    gameOver = false;
+    Gameboard.render();
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+      square.addEventListener("click", handleClick);
+    });
+  };
+
+  const handleClick = (event) => {
+    if (gameOver) return;
+    let index = parseInt(event.target.id.split("-")[1]);
+
+    if (Gameboard.getGameboard()[index]) return;
+
+    Gameboard.update(index, players[currentPlayerIndex].mark);
+
+    if (checkForWin(Gameboard.getGameboard())) {
+      gameOver = true;
+      displayController.renderMessage(
+        `${players[currentPlayerIndex].name} winsS`
+      );
+    } else if (checkForTie(Gameboard.getGameboard())) {
+      gameOver = true;
+      displayController.renderMessage(`It's a tie!`);
+    }
+    currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
+  };
+
+  const restart = () => {
+    for (let i = 0; i < 9; i++) {
+      Gameboard.update(i, "");
+    }
+    Gameboard.render();
+    gameOver = false;
+    document.querySelector("#message").innerHTML = "";
+  };
+
+  return { start, restart, handleClick };
+})();
+
+function checkForWin(board) {
+  const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2],
+  ];
+
+  for (let i = 0; i < winningCombinations.length; i++) {
+    const [a, b, c] = winningCombinations[i];
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      return true;
+    }
   }
-  boardDiv.addEventListener("click", clickHandlerBoard);
-
-  // initial render
-  updateScreen();
-
-  // We don't need to return anything from this module because everything is encapsulated inside this screen controller.
+  return false;
 }
 
-ScreenController();
+function checkForTie(board) {
+  return board.every((cell) => cell);
+}
+
+const restartButton = document.querySelector("#btn-restart");
+restartButton.addEventListener("click", () => {
+  Game.restart();
+});
+
+const startButton = document.querySelector("#btn-start");
+startButton.addEventListener("click", () => {
+  Game.start();
+});
